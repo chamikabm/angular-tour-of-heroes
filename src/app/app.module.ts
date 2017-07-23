182ca0c03aed4e0b4d210d8fe00453a53b1aa77e
Chamika Bandara <chamika.bandara@trycake.com>
2017-07-23 15:14:52 +0530

    Added dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+import { AppRoutingModule }     from './app-routing.module';
+
+import { AppComponent }        from './app.component';
+import { DashboardComponent, }        from './dashboard.component';
 import { HeroDetailComponent } from './hero-detail.component';
-import { AppComponent } from './app.component';
+import { HeroesComponent }     from './heroes.component';
+import { HeroService }         from './hero.service';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeroDetailComponent
-  ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent
+  ],
+  providers: [
+    HeroService
+  ],
+  bootstrap: [ AppComponent ]
 })
 
-export class AppModule { }
+export class AppModule {}
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -16,4 +16,9 @@ export class HeroService {
       setTimeout(() => resolve(this.getHeroes()), 2000);
     });
   }
+
+  getHero(id: number): Promise<Hero> {
+    //noinspection TypeScriptUnresolvedFunction
+    return this.getHeroes().then(heroes => heroes.find(hero => hero.id === id));
+  }
 }
